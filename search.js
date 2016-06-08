11c94852f2c74aaaa43d3df6948ace3cbe241a09
lucas wilson <koka-noodles@users.noreply.github.com>
2016-06-08 13:53:56 +0100

    Update search.js

--- search.js
+++ search.js
@@ -12,22 +12,6 @@ var wiPlaynum = 0;
 var playnum = 0; // delete after everyone has one
 
 
-var MongoClient = require('mongodb').MongoClient, assert = require('assert');
-  // Connection URL
-  var url = 'mongodb://<lucasrowanwilson@gmail.com>:<MLCannondale11>@ds021943.mlab.com:21943/noodles';
-  // Use connect method to connect to the Server
-  MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-
-  db.close();
-  });
-
-
-
-
-
-
 // Your use of the YouTube API must comply with the Terms of Service:
 // https://developers.google.com/youtube/terms
 // Helper function to display JavaScript value on HTML page.
