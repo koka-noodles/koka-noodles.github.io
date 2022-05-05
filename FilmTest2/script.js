b03be1a30305dacbbd5f1b4d5a781be8abe07072
lucas wilson <koka-noodles@users.noreply.github.com>
2022-05-05 11:16:43 +0100

    dsad

---
+++ FilmTest2/script.js
@@ -0,0 +1,18 @@
+const APIURL = "https://api.themoviedb.org/3/discover/movie?api_key=4bdff0b7b1503da2954be3244d740dfe";
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=4bdff0b7b1503da2954be3244d740dfe&query=";
+
+
+const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
+
+function getMovies(){
+  console.log("Yeah");
+
+
+  
+
+
+}
