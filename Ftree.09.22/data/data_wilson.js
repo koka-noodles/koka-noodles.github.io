a7c5804430cb86dc11e76802e70d612fcfe04401
lucas wilson <koka-noodles@users.noreply.github.com>
2022-09-16 16:29:30 +0100

    added gen minus two

--- Ftree.09.22/data/data_wilson.js
+++ Ftree.09.22/data/data_wilson.js
@@ -1,66 +1,169 @@
 data = {
     "start":"id1",
     "persons": {
-        "id1": { "id": "id1", "name": "Lucas Wilson", "birthyear": 1976, "deathyear": 2099, "own_unions": ["u1"], "parent_union": "u2", "birthplace":"Wik", "deathplace":"The Moon"},
-        "id2": { "id": "id2", "name": "Clare F", "birthyear": 1978, "deathyear": 2099, "own_unions": ["u1"], "birthplace":"Cork", "deathplace":"The Moon"},
-        "id3": { "id": "id3", "name": "Morrow Fletcher-Wilson", "birthyear": 2018, "deathyear": 2199, "own_unions": [], "parent_union": "u1", "birthplace":"Dublin", "deathplace":"Neptune"},
-        "id4": { "id": "id4", "name": "Judy Russell", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u2"], "birthplace":"UK", "deathplace":"Neptune"},
+
+        //gen -2
+        "id19": { "id": "id19", "name": "G W Russell", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u7"], "birthplace":"UK", "deathplace":"Neptune"},
+        "id20": { "id": "id20", "name": "Ann Willy", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u7"], "birthplace":"UK", "deathplace":"Neptune"},
+
+        //gen -1
+        "id4": { "id": "id4", "name": "Judy Russell", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u2"], "parent_union": "u7", "birthplace":"UK", "deathplace":"Neptune"},
         "id5": { "id": "id5", "name": "David Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u2", "u3"], "birthplace":"UK", "deathplace":"Neptune"},    
-        "id6": { "id": "id6", "name": "Rama Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u4"], "birthplace":"Ireland", "deathplace":"Neptune"},    
-        "id7": { "id": "id7", "name": "Jason Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [""], "birthplace":"Ireland", "deathplace":"Neptune"},    
-        "id8": { "id": "id8", "name": "Chokole Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [""], "birthplace":"Ireland", "deathplace":"Neptune"},   
         "id9": { "id": "id9", "name": "Fiona Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u3"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id16": { "id": "id16", "name": "Angela Fletcher", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u6"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id17": { "id": "id17", "name": "Ian Fletcher", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u6"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        
+        //Gen 0
+        "id1": { "id": "id1", "name": "Lucas Wilson", "birthyear": 1976, "deathyear": 2099, "own_unions": ["u1"], "parent_union": "u2", "birthplace":"Wik", "deathplace":"The Moon"},
+        "id2": { "id": "id2", "name": "Clare F", "birthyear": 1978, "deathyear": 2099, "own_unions": ["u1"], "parent_union": "u6", "birthplace":"Cork", "deathplace":"The Moon"},
+        "id6": { "id": "id6", "name": "Rama Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u4"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id7": { "id": "id7", "name": "Jason Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id8": { "id": "id8", "name": "Chokole Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u5"], "birthplace":"Ireland", "deathplace":"Neptune"},   
         "id10": { "id": "id10", "name": "Danny Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "parent_union": "u3", "birthplace":"Ireland", "deathplace":"Neptune"},    
         "id11": { "id": "id11", "name": "Sarah Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u4"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id15": { "id": "id15", "name": "Jason McC", "birthyear": 1950, "deathyear": 2199, "own_unions": ["u5"], "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id18": { "id": "id18", "name": "Jonathan Fletcher", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "parent_union": "u6", "birthplace":"Ireland", "deathplace":"Neptune"},    
+        
+        //Gen 1
+        "id3": { "id": "id3", "name": "Morrow Fletcher-Wilson", "birthyear": 2018, "deathyear": 2199, "own_unions": [], "parent_union": "u1", "birthplace":"Dublin", "deathplace":"Neptune"},     
         "id12": { "id": "id12", "name": "Thorjborn Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "parent_union": "u4", "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id13": { "id": "id13", "name": "Robin Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "parent_union": "u5", "birthplace":"Ireland", "deathplace":"Neptune"},    
+        "id14": { "id": "id14", "name": "Orin Wilson", "birthyear": 1950, "deathyear": 2199, "own_unions": [], "parent_union": "u5", "birthplace":"Ireland", "deathplace":"Neptune"},    
+        
+        // index is at 20
+        
 
     },
     "unions": {
-        //lucas and clare 
-        "u1": { "id": "u1", "partner": ["id1", "id2"], "children": ["id3"] },
+        
+        //Gen -2
+
+        //G W Russell and Ann Willy
+        "u7": { "id": "u7", "partner": ["id19", "id20"], "children": ["id4"] },
+
+        
+        //Gen -1
+
+
         //Dave and Judy
         "u2": { "id": "u2", "partner": ["id4", "id5"], "children": ["id1"] },
         //Dave and Fiona
         "u3": { "id": "u3", "partner": ["id9", "id5"], "children": ["id10"] },
+        //Ian and Angela
+        "u6": { "id": "u6", "partner": ["id16", "id17"], "children": ["id2"] },
+
+        //Gen 0
+
+
+        //lucas and clare 
+        "u1": { "id": "u1", "partner": ["id1", "id2"], "children": ["id3"] },
+        
         //Rama and Sarah
         "u4": { "id": "u4", "partner": ["id6", "id11"], "children": ["id12"] },
         
+        //Coco and Jason
+        "u5": { "id": "u5", "partner": ["id8", "id15"], "children": ["id13", "id14"] },
+        
+        
+        //index is at 7
         
     },
     "links": [
-        //lucas wilson -> to clare Fletcher
-        ["id1", "u1"],
-        //Clare Fletcher -> to Lucas Wilson
-        ["id2", "u1"],
-        //Morrow wilson -> Lucas & Clare 
-        ["u1", "id3"],
+
+
+        //Gen -2
         
-        //Dave wilson -> Judy & dave 
+        //G W Russell -> G W Russell & Ian union
+        ["id19", "u7"],
+        //Ann Willy -> G W Russell & Ann Willy
+        ["id20", "u7"],
+
+        //Gen -0 -> -1
+
+        //G W Russell & Ian union -> to Judy Russell 
+        ["u7", "id4"],
+
+
+        //Gen -1
+
+        //Ian Fletcher -> Angela & Ian union
+        ["id17", "u6"],
+        //Angela Fletcher -> Angela & Ian union
+        ["id16", "u6"],
+
+        //Dave wilson -> Judy & dave union
         ["id5", "u2"],
-        //Judy Russell -> dave & judy 
+        //Judy Russell -> dave & judy union
         ["id4", "u2"],
 
-        //Dave wilson -> Fiona wilson
-        ["id5", "u3"],
-        //Fiona wilson -> Dave wilson
-        ["id9", "u3"],
-        //Danny wilson -> Dave & Fiona 
-        ["u3", "id10"],
+         //Dave wilson -> Dave & Fiona union
+         ["id5", "u3"],
+         //Fiona wilson -> Dave & Fiona union
+         ["id9", "u3"],
 
-        //judy & dave -> to lucas wilson
+
+        //Gen -1 -> 0
+
+        //judy & dave union -> to lucas wilson
         ["u2", "id1"],
-        //judy & dave -> to coco wilson
+        //judy & dave union -> to coco wilson
         ["u2", "id8"],
-        //judy & dave -> to Jason wilson
+        //judy & dave union -> to Jason wilson
         ["u2", "id7"],
-        //judy & dave -> to Rama wilson
+        //judy & dave union -> to Rama wilson
         ["u2", "id6"],
+         //Clare fletcher -> Angela & Ian union
+         ["u6", "id2"],
+         //Danny wilson -> Dave & Fiona union
+         ["u3", "id10"],
+         //Jonny Fletcher -> Angela & Ian union
+         ["u6", "id18"],
 
-        //Rama wilson -> to Sarah wilson
+
+        //gen 0
+
+        //lucas wilson -> to clare & lucas union
+        ["id1", "u1"],
+        //Clare Fletcher -> to Lucas & Clare union
+        ["id2", "u1"],
+        //Rama wilson -> to rama & sarah union
         ["id6", "u4"],
-        //Sarah wilson -> to rama wilson
+        //Sarah wilson -> to rama & sarah union
         ["id11", "u4"],
-        //Thorjborn -> to rama & sarah
+        //CoCo wilson -> JasonMC & Coco union 
+        ["id8", "u5"],
+        //Jason MC -> JasonMC & Coco union 
+        ["id15", "u5"],
+       
+
+
+        //Gen 1 -> 0
+
+        //Morrow wilson -> Lucas & Clare union
+        ["u1", "id3"],
+        //Thorjborn -> to rama & sarah union
         ["u4", "id12"],
+        //Robin wilson -> JasonMC & Coco union 
+        ["u5", "id13"],
+        //Orin wilson -> JasonMC & Coco union 
+        ["u5", "id14"],
+
+        
+        
+        
+        
+
+        
+
+       
+        
+
+        
+
+        
+        
+
+        
+        
     ]
 }
