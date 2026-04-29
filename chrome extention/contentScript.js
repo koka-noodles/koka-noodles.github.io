d4be6aca4208d4c7709ca6d8cc10b3a98617b942
koka-noodles <lucasrowanwilson@gmail.com>
2026-04-29 14:14:32 +0100

    added a css editor

---
+++ chrome extention/contentScript.js
@@ -0,0 +1,8 @@
+// document.getElementById("title").style.color = "magenta";
+// document.getElementById("title").style.height = "500px";
+ 
+// document.getElementsByClassName("language-border").style.color = "magenta";
+
+// document.getElementsByClassName("scroll-bar-design ng-scope no-padding").style.max-height = "400px";
+
+
